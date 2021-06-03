0294d974df2ba4d3dfbf2ca76346c90fd8ab685b
deepak <kushwahadeepakchange121@gmail.com>
2021-06-03 10:34:34 +0530

    assignment1

--- block-BJaaco/code/index.js
+++ block-BJaaco/code/index.js
@@ -3,21 +3,49 @@ let numbers = [6, 8, 10, 12, 43, 56, 98];
 let userIds = [1230, 234, 1278, 984, 763, 900];
 
 // 1. Add all the values of numbers and userIds array into the new newly created array named `collection`
-let collection=[]
+let collection=[];
 
-for(let number of numbers+userIds)
-console.log(number);
+for(let number of numbers){
+  collection.push(number);
+}
+
+for(let user of userIds){
+  collection.push(user);
+}
+console.log(collection);
+ 
+
 
 // 2. Add all the even numbers from both arrays numbers and userIds into a newly created array named `evenCollection`
+let evenCollection=[];
+for(let even of numbers){
 
-if(numbers,userIds%2==0){
-  console.log("even")
+if(even%2==0){
+  evenCollection.push(even);
 }
-else{
-  console.log("odd")
 }
+for(let Ids of userIds){
+
+if(Ids%2==0){
+  evenCollection.push(Ids);
+}
+}
+console.log(evenCollection);
 // 3. Add all the odd numbers from both arrays numbers and userIds into a newly created array named `oddCollection`
+let evenCollection=[];
+for(let even of numbers){
 
+if(even%2!==0){
+  evenCollection.push(even);
+}
+}
+for(let Ids of userIds){
+
+if(Ids%2!==0){
+  evenCollection.push(Ids);
+}
+}
+console.log(evenCollection);
 /*
   @param means parameter
 
@@ -34,16 +62,20 @@ else{
     times(5); // ['test', 'test', 'test', 'test', 'test']
 */
 
-function times() {
-  // Your code
+function times(Time,character="test") {
+  if(Time<1) return[];
+ let finalArray=[];
+ for(let i=0;i<Time; i++){
+   finalArray.push(character);
+ }
+return finalArray;
 }
 
 // Uncomment the code below and test the output
-
-// console.log(times(5, 'c')); // ['c', 'c', 'c', 'c', 'c']
-// console.log(times(2, 'a')); // ['a', 'a']
-// console.log(times(0)); // []
-// console.log(times(5)); // ['test', 'test', 'test', 'test', 'test']
+ console.log(times(5, 'c')); // ['c', 'c', 'c', 'c', 'c']
+console.log(times(2, 'a')); // ['a', 'a']
+ console.log(times(0)); // []
+ console.log(times(5)); // ['test', 'test', 'test', 'test', 'test']
 
 /*
 
@@ -58,14 +90,20 @@ function times() {
     revert(['Ryan', 'John', 'Bran']); //['Bran', 'John', 'Ryan']
 */
 
-function revert() {
-  // your code
+function revert(array) {
+  let final=[];
+  for(let i=array.length-1;i>=0;i--){
+    final.push(array[i]);
+  }
+  return final;
 }
 
+
+
 // Uncomment the code below and test the output
-// console.log(revert([1, 2, 3, 4])); // [4, 3, 2, 1]
-// console.log(revert(['a', 'd', 'c', 'b'])); // ['b', 'c', 'd', 'a']
-// console.log(revert(['Ryan', 'John', 'Bran'])); //['Bran', 'John', 'Ryan']
+ console.log(revert([1, 2, 3, 4])); // [4, 3, 2, 1]
+ console.log(revert(['a', 'd', 'c', 'b'])); // ['b', 'c', 'd', 'a']
+ console.log(revert(['Ryan', 'John', 'Bran'])); //['Bran', 'John', 'Ryan']
 
 /*
 
@@ -80,9 +118,18 @@ function revert() {
     clear(['Ryan', null, 0,  'John', 'Bran']); //['Bran', 'John', 'Ryan']
 */
 
-function clear() {
-  // your code
+function clear(array) {
+  let final=[];
+  for(let value of array){
+    if(typeof value===number){
+      final.push(array);
+
+    }else{
+      delete array
+    }
+  }
 }
+  
 
 // Uncomment the code below and test the output
 // console.log(clear([1, 2, 3, 4, '', 0, null, undefined])); // [4, 3, 2, 1]
